9201b2db0228cc6970b32893b52eee4cb036e6b0
Lijun Tang <hercule2400@gmail.com>
2015-05-16 22:32:49 -0500

    working version

--- pa4/bayes.py
+++ pa4/bayes.py
@@ -7,7 +7,23 @@
 
 # all divide operation will result in float number
 from __future__ import division
+from math import log
 import math, os, pickle, re
+
+def unique(words):
+    i = 0
+    j = 1
+    length = 1
+    while j < len(words):
+        if words[i] == words[j]:
+            j += 1
+            continue
+        else:
+            words[i+1] = words[j]
+            i += 1
+            j += 1
+            length += 1
+    return length
 
 class Bayes_Classifier:
 
@@ -16,22 +32,44 @@ class Bayes_Classifier:
         cache of a trained classifier has been stored, it loads this cache.  Otherwise, 
         the system will proceed through training.  After running this method, the classifier 
         is ready to classify input text."""
-        class_dict = {}
+        self.class_dict = {}
         self.positive_word = {}
         self.negative_word = {}
         if os.path.exists("positive_word.pickle")and os.path.exists("negative_word.pickle") and os.path.exists("class_dict.pickle"):
-            class_dict = self.load("class_dict.pickle")
+            self.class_dict = self.load("class_dict.pickle")
             self.positive_word = self.load("positive_word.pickle")
             self.negative_word = self.load("negative_word.pickle")
         else:
-            class_dict, self.positive_word, self.negative_word = self.train()
+            self.class_dict, self.positive_word, self.negative_word = self.train() 
 
-        self.pos_num = class_dict["positive"]
-        self.neg_num = class_dict["negative"]
+
+        self.pos_num = self.class_dict["positive"]
+        self.neg_num = self.class_dict["negative"]
         self.total_num = self.pos_num + self.neg_num
 
         self.prob_pos = self.pos_num / self.total_num
         self.prob_neg = self.neg_num / self.total_num
+
+        self.pos_word_count = 0
+        for word in self.positive_word:
+            self.pos_word_count += self.positive_word[word]
+
+        self.neg_word_count = 0
+        for word in self.negative_word:
+            self.neg_word_count += self.negative_word[word]
+
+        words = []
+        for word in self.positive_word:
+            words.append(word)
+        for word in self.negative_word:
+            words.append(word)
+
+        words.sort()
+
+        self.voc_length = unique(words)
+        #print self.voc_length
+        #print self.pos_word_count
+        #print self.neg_word_count
 
     def train(self):   
         """Trains the Naive Bayes Sentiment Classifier."""
@@ -94,6 +132,35 @@ class Bayes_Classifier:
         """Given a target string sText, this function returns the most likely document
         class to which the target string belongs (i.e., positive, negative or neutral).
         """
+        tokens = self.tokenize(sText)
+        
+        prob = {}
+        prob["positive"] = log(self.prob_pos, 2)
+        prob["negative"] = log(self.prob_neg, 2)
+    
+        for c in self.class_dict:
+            for word in tokens:
+                p = None
+                if c == "positive":
+                    if word in self.positive_word:
+                        p = (self.positive_word[word] + 1) / (self.pos_word_count + self.voc_length)
+                    else:
+                        p = 1 / (self.pos_word_count + self.voc_length)
+
+                else:
+                    if word in self.negative_word:
+                        p = (self.negative_word[word] + 1) / (self.neg_word_count + self.voc_length)
+                    else:
+                        p = 1 / (self.neg_word_count + self.voc_length)
+                
+                prob[c] += log(p, 2)
+         
+        if prob["negative"] > prob["positive"]:
+            return "negative"
+        elif prob["positive"] > prob["negative"]:
+            return "positive"
+        else:
+            return "neutral"
 
     def loadFile(self, sFilename):
         """Given a file name, return the contents of the file as a string."""
@@ -143,3 +210,7 @@ class Bayes_Classifier:
 
 if __name__ == "__main__":
     c = Bayes_Classifier()
+    text = c.loadFile("./test_dir/movies-1-11508.txt")
+    print c.classify(text)
+    text = c.loadFile("./test_dir/movies-5-110.txt")
+    print c.classify(text)
